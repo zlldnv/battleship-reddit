b17f0822a8b0721e76cfd1bc458b8dc23b0a201c
anton z <zalaldinov@icloud.com>
2021-05-07 15:03:01 +0300

    added images to board fields

--- src/components/BoardField/BoardField.js
+++ src/components/BoardField/BoardField.js
@@ -1,11 +1,29 @@
 import "./BoardField.css";
+import Hit from "assets/Hit.png";
+import Miss from "assets/Miss.png";
 
-const BoardField = () => {
+const BoardField = ({ type }) => {
   const onFire = () => {
     console.log("FIRE!");
   };
 
-  return <div className="field" onClick={onFire}></div>;
+  const filedImage = () => {
+    if (type === "hit") {
+      return Hit;
+    }
+
+    if (type === "miss") {
+      return Miss;
+    }
+  };
+
+  return (
+    <div className="field" onClick={onFire}>
+      {filedImage() && (
+        <img className="fieldImage" src={filedImage()} alt="fieldImage" />
+      )}
+    </div>
+  );
 };
 
 export default BoardField;
