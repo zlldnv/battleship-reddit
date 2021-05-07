99d7306fe68683f07cafd04833f403a75d3f8b07
anton z <zalaldinov@icloud.com>
2021-05-07 15:34:30 +0300

    add mutation to the ship when player fires

--- src/components/BoardField/BoardField.js
+++ src/components/BoardField/BoardField.js
@@ -1,22 +1,36 @@
 import { useSetRecoilState } from "recoil";
-import { boardState } from "recoil/atoms";
+import { boardState, shipsState } from "recoil/atoms";
 import Miss from "assets/Miss.png";
 import Hit from "assets/Hit.png";
 
 import "./BoardField.css";
 
-const BoardField = ({ type, rowIndex, fieldIndex }) => {
+const BoardField = ({ id, type, rowIndex, fieldIndex }) => {
   const setBoard = useSetRecoilState(boardState);
+  const setShips = useSetRecoilState(shipsState);
 
   const onFire = () => {
-    setBoard((board) => {
-      const arrayCopy = [...board.map((row) => [...row])];
-      arrayCopy[rowIndex][fieldIndex] = {
-        ...arrayCopy[rowIndex][fieldIndex],
-        type: arrayCopy[rowIndex][fieldIndex].id !== null ? "hit" : "miss",
-      };
-      return arrayCopy;
-    });
+    if (type === "notTouched") {
+      setBoard((board) => {
+        const arrayCopy = [...board.map((row) => [...row])];
+        arrayCopy[rowIndex][fieldIndex] = {
+          ...arrayCopy[rowIndex][fieldIndex],
+          type: arrayCopy[rowIndex][fieldIndex].id !== null ? "hit" : "miss",
+        };
+        return arrayCopy;
+      });
+
+      if (id !== null) {
+        setShips((shipsValue) => {
+          const shipsCopy = [...shipsValue];
+          shipsCopy[id] = {
+            ...shipsCopy[id],
+            killed: shipsCopy[id].killed + 1,
+          };
+          return shipsCopy;
+        });
+      }
+    }
   };
 
   const filedImage = () => {
